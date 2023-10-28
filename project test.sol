7296d2013f7c3eb80706f57ceef9b35501aa482c
jy2020smu <149249734+jy2020smu@users.noreply.github.com>
2023-10-28 22:35:42 +0800

    project test.sol

--- project test.sol
+++ project test.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.5.8;
 contract HDBRentalContract {
 
     // Parties involved
-    address public landlordAddress; 
-    address public tenantAddress; 
+    address public landlordAddress; // 
+    address public tenantAddress; //
 
     // HDB Property Details
 	struct HDB {
@@ -16,22 +16,24 @@ contract HDBRentalContract {
 		bool rented;
 		string furnishing; // unfurnished/ semi-furnished/ fully-furnished w details
 		uint rentInterval; // interval in days for the rent amount
-		uint rentAmount; // HDB rent value
+		uint rentAmount; // 
 		uint tenantWarning; // tracks the count for warning given to tenant
 		uint warningLimit; // threshold limit for warning. Once excess tenant can be dismissed
 		uint dueDate; // unit timestamp for the dueDate
-		address tenant; // tenant wallet address
+		address tenantAddress; // tenant wallet address
 		}
+
 
 // Payment
 	struct MonthlyRentStatus {
-		uint amount; // Monthly rent amount to be paid (includes utilities)
+		uint rentAmount; // Monthly rent amount to be paid (includes utilities)
 		uint amountDeposit; // Deposit and dispute amount if furniture spoil
 		uint validationDate; // unix timestamp at which the rent was paid
-		bool status; // rent status
+		bool rented; // rent status
 		string disputeStatement; //a statement to indicate dispute amount if furniture spoil
 
 	}
+
 
 // Lease term
 	struct leaseTerm {
@@ -42,13 +44,15 @@ contract HDBRentalContract {
 	}
 
 
+
 // Rental status
     bool public isLeaseActive; // True: not available False: available for rent
+
 
  
 	HDB public hdb; // instance of HDB struct
 	address payable public landlord; // wallet address of contract/property owner
-	uint private warningTime = 0; // unix timestamp when the tenant gets warned.
+	uint private warningTime = 0; // timestamp when the tenant gets warned.
 	mapping(string => bool) public months;
 	mapping(address => bool) public tenantRegistry; // storage for users other than owner registered as a prospect.
 	mapping(string => MonthlyRentStatus) public rentInStore; // storage for rental status
@@ -63,12 +67,12 @@ contract HDBRentalContract {
  // constructor to initialise the contract
 	constructor(string memory name, string memory addr, string memory furnishing, uint room, uint rentInterval, uint rentAmount, uint warningLimit) public {
 		hdb = HDB({name: name, addr: addr, furnishing: furnishing, room: room, rented: false, rentInterval: rentInterval, rentAmount: rentAmount,
-		warningLimit: warningLimit, tenantWarning: 0, dueDate: 0, tenant: address(0) });
+		warningLimit: warningLimit, tenantWarning: 0, dueDate: 0, tenantAddress: address(0) });
 		landlord = msg.sender;
 		setMonths();
 	}
 
-		function setMonths() private {
+	function setMonths() private {
 	    months["Jan"] = true;
 	    months["Feb"] = true;
 	    months["Mar"] = true;
@@ -82,7 +86,6 @@ contract HDBRentalContract {
 	    months["Nov"] = true;
 	    months["Dec"] = true;
 	}
-
 
 	modifier onlyOwner {
         require(msg.sender == landlord, "Only owner is authorized");
@@ -104,17 +107,16 @@ contract HDBRentalContract {
 		_;
 	}
 
-
-	// rent payment, which also sets the next due date
+// rent payment, which also sets the next due date
 	function payRent(string calldata  month) external payable nonTenant allowedMonths(month) returns (bool success) {
-		require(rentInStore[month].status == false,
+		require(rentInStore[month].rented == false,
         "Rent already paid"); // use require & not if statement, since function is payable & transaction should get reverted in invalid case.
 		require(msg.sender == tenantAddress, 
 		"Only tenant can pay rent");
 		require(msg.value == hdb.rentAmount,
         "Incorrect rent amount");
-		rentInStore[month].amount = msg.value;
-		rentInStore[month].status = true;
+		rentInStore[month].rentAmount = msg.value;
+		rentInStore[month].rented = true;
 		rentInStore[month].validationDate = now;
 		hdb.dueDate = (block.number + (hdb.rentInterval * 6400 *15)); // calculate end time as 6400 blocks from the current block and a block time of 15 seconds
 		hdb.tenantWarning = 0;
@@ -126,21 +128,21 @@ contract HDBRentalContract {
 	function getRentStatus(string calldata month) external allowedMonths(month) view returns(uint amount, uint date, bool status) {
 		MonthlyRentStatus memory rentStatus = rentInStore[month];
 		return (
-			rentStatus.amount,
+			rentStatus.rentAmount,
 			rentStatus.validationDate,
-			rentStatus.status
+			rentStatus.rented
 		);
 
 	}
 
 	// an api for property owner to withdraw the rent amount from smart contract.
 	function withdrawRent(string calldata month) external onlyOwner allowedMonths(month) returns (bool success) {
-		require(rentInStore[month].amount <= address(this).balance,
+		require(rentInStore[month].rentAmount <= address(this).balance,
          "Insufficient contract balance"); // This is a must condition.
-		uint balance = rentInStore[month].amount;
+		uint balance = rentInStore[month].rentAmount;
 		if(balance == hdb.rentAmount) {
 			landlord.transfer(balance);
-			rentInStore[month].amount = 0;
+			rentInStore[month].rentAmount = 0;
 		}
 		emit RentWithdrawn(month, hdb.rentAmount); // confirming the rent withdraw transaction.
 		return true;
@@ -149,7 +151,7 @@ contract HDBRentalContract {
 	// when owner wants to warn the tenant about pending rent payment.
 	function warnTenant(string calldata month) external onlyOwner allowedMonths(month) returns (bool success) {
 		require(hdb.rented == true, "Tenant doesn't exists");
-		if((rentInStore[month].status == false) && (now > hdb.dueDate) && ((now - warningTime) > 172800000)) {
+		if((rentInStore[month].rented == false) && (now > hdb.dueDate) && ((now - warningTime) > 172800000)) {
 			hdb.tenantWarning++;
 			warningTime = now;
 			emit TenantWarning(month, hdb.tenantWarning);
@@ -161,13 +163,15 @@ contract HDBRentalContract {
 // when warning limit has been crossed & owner wants to dismiss the tenant
 	function dismissTenant() external onlyOwner returns (bool success) {
 		require(hdb.tenantWarning > hdb.warningLimit,"Warning limit is below threshold");
-		hdb.tenant = address(0);
+		hdb.tenantAddress = address(0);
 		hdb.rented = false;
 		hdb.tenantWarning = 0;
 		hdb.dueDate = 0;
 		emit DismissTenantConfirmed(true);
 		return true;
 	}
+
+
  // Function to end the lease
     function endLease() public {
         require(msg.sender == landlordAddress, "Only the landlord can end the lease");
@@ -178,14 +182,13 @@ contract HDBRentalContract {
         isLeaseActive = false;
     }
 
-  // Function to renew the lease
+// Function to renew the lease
     function renewLease(uint newEndDate) view  public {
         require(msg.sender == tenantAddress, "Only the tenant can renew the lease");
         require(isLeaseActive, "Lease must be active to renew");
 
         // Update the lease end date
-        newEndDate = newEndDate;
+       newEndDate = newEndDate;
 
     }
-
 }
